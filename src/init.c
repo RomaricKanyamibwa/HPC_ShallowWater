10b8f5cf01e3f738acdead0ebf06e7e8a39a67fc
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-04-05 23:06:47 +0200

    Almost bande

--- src/init.c
+++ src/init.c
@@ -16,11 +16,11 @@ void gauss_init(void) {
 	(exp(- pow((i * dx - gmx) / gsx, 2) / 2.)) *
 	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
 	//ATTENTION OPTI LOCAL_SIZEX
-	if(i<=local_size_x)
-	{
-        tmp=(size_x/NP*my_rank);
-        printf("P#%d:tmp=%lf\n",my_rank,tmp);
-	}
+//	if(i<=local_size_x)
+//	{
+//        tmp=(size_x/NP*my_rank);
+//        printf("P#%d:tmp=%lf\n",my_rank,tmp);
+//	}
 	HFIL_LOCAL(0, i, j) = height *
 	(exp(- pow(((i+tmp-1) * dx - gmx) / gsx, 2) / 2.)) *
 	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;

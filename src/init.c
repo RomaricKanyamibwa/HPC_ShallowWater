fa1b1c961031e25c2d402e8f036fcf7952a90124
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-04-05 15:26:07 +0200

    init

--- src/init.c
+++ src/init.c
@@ -3,6 +3,7 @@
 
 void gauss_init(void) {
   double gmx, gmy, gsx, gsy;
+  double tmp=0.0;
 
   gmx = size_x * dx / 2 ;
   gmy = size_y * dy / 2 ;
@@ -15,8 +16,11 @@ void gauss_init(void) {
 	(exp(- pow((i * dx - gmx) / gsx, 2) / 2.)) *
 	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
 	//ATTENTION OPTI LOCAL_SIZEX
+	if((i<local_size_x))
+        tmp=(size_x/NP*my_rank);
+    printf("tmp=%lf\n",tmp);
 	HFIL_LOCAL(0, i, j) = height *
-	(exp(- pow(((i+(i<local_size_x)*(size_x/NP*my_rank)) * dx - gmx)/ gsx, 2) / 2.)) *
+	(exp(- pow(((i+tmp) * dx - gmx) / gsx, 2) / 2.)) *
 	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
     }
   }

2753bfdc05faca6480b807ab41690d2e4bb3b7c9
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-04-07 01:37:01 +0200

    correction par bande 01h36

--- src/init.c
+++ src/init.c
@@ -21,9 +21,10 @@ void gauss_init(void) {
         tmp=(size_x/NP*my_rank);
         printf("P#%d:tmp=%lf\n",my_rank,tmp);
 	}
-	HFIL_LOCAL(0, i+1*(i<=size_x/NP&&0!=my_rank), j) = height *
-	(exp(- pow(((i+tmp) * dx - gmx) / gsx, 2) / 2.)) *
-	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
+//	HFIL_LOCAL(0, i+1*(i<=size_x/NP&&0!=my_rank), j) = height *
+//	(exp(- pow(((i+tmp) * dx - gmx) / gsx, 2) / 2.)) *
+//	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
+    gauss_init_loc(my_rank,NP);
     }
   }
 }

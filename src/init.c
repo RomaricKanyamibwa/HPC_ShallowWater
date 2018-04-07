cc926ecdeda7393e565888e4c6a5c138c78a47c1
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-04-07 02:21:47 +0200

    correction de la initialisation 02h20

--- src/init.c
+++ src/init.c
@@ -18,11 +18,11 @@ void gauss_init(void) {
 	(exp(- pow((i * dx - gmx) / gsx, 2) / 2.)) *
 	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;
 	//ATTENTION OPTI LOCAL_SIZEX
-	if(i<size_x/NP+1)
-	{
-        tmp=(size_x/NP*my_rank)/*-1*(i>0)*(my_rank!=0)*/;
-        //printf("P#%d:tmp=%lf\n",my_rank,tmp);
-	}
+//	if(i<size_x/NP+1)
+//	{
+//        tmp=(size_x/NP*my_rank)/*-1*(i>0)*(my_rank!=0)*/;
+//        //printf("P#%d:tmp=%lf\n",my_rank,tmp);
+//	}
 //	HFIL_LOCAL(0, i-1*(my_rank!=0)*(i<size_x/NP)*(i>0), j) = height *
 //	(exp(- pow(((i+tmp) * dx - gmx) / gsx, 2) / 2.)) *
 //	(exp(- pow((j * dy - gmy) / gsy, 2) / 2.)) ;

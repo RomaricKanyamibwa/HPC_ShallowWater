df76e4612aff44980ff7e8df62dfe6b401b1e631
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-04-08 05:00:47 +0200

    MPI_IO 5h00

--- src/export.c
+++ src/export.c
@@ -54,12 +54,12 @@ void export_step_mpi(MPI_File *f, int t) {
     offset = my_rank*size_x/NP *size_y + (t)*size_x*size_y ;
   	//fwrite((void *)&HFIL(t, 0, 0), sizeof(double), size_x * size_y, f);
   	MPI_File_write_at_all(*f, offset, (void *)&HFIL(t, 0, 0),
-                  size_x*size_y, MPI_DOUBLE, &status)
+                  size_x*size_y, MPI_DOUBLE, &status);
 	//printf("k2\n");
 }
 
 void finalize_export_mpi(MPI_File *f) {
 	printf("CLOSE\n");
 	//if(my_rank==0)
-    MPI_File_close(&fh);
+    MPI_File_close(fh);
 }

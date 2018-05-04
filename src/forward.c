36b0e0d8db3c9507729c0a171c57062ac2ef5f85
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-05 01:56:59 +0200

    Gathering bloc v16

--- src/forward.c
+++ src/forward.c
@@ -475,12 +475,12 @@ void forward_bloc(void) {
             {
                 for(int j=0;j<NP;j++)
                 {
-                    printf("i:%d,j:%d\n",i,j);
-                    printf("(j/NbCol)*size_x/NbLi:%d\n",(j/NbCol)*size_x/NbLi);
-                    printf("i+(j/NbCol)*size_x/NbLi:%d\n",(j/NbCol)*size_x/NbLi);
-                    printf("(j mod NbCol)*size_y/NbCol:%d\n",(j%NbCol)*size_y/NbCol);
-                    printf("j*size_y/NbCol*size_x/NbLi:%d\n",j*size_y/NbCol*size_x/NbLi);
-                    printf("i*size_y/NbCol:%d\n",i*size_y/NbCol);
+//                    printf("i:%d,j:%d\n",i,j);
+//                    printf("(j/NbCol)*size_x/NbLi:%d\n",(j/NbCol)*size_x/NbLi);
+//                    printf("i+(j/NbCol)*size_x/NbLi:%d\n",(j/NbCol)*size_x/NbLi);
+//                    printf("(j mod NbCol)*size_y/NbCol:%d\n",(j%NbCol)*size_y/NbCol);
+//                    printf("j*size_y/NbCol*size_x/NbLi:%d\n",j*size_y/NbCol*size_x/NbLi);
+//                    printf("i*size_y/NbCol:%d\n",i*size_y/NbCol);
                     memcpy(&HFIL(t, i+(j/NbCol)*size_x/NbLi,(j%NbCol)*size_y/NbCol)
                            ,hphy_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
                 }

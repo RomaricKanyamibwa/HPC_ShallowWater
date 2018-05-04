16c4b9c9026e0612c44335bf874c5b7d160c01c1
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-04 17:26:39 +0200

    Gathering bloc v5

--- src/forward.c
+++ src/forward.c
@@ -448,18 +448,18 @@ void forward_bloc(void) {
     }
     //for(k=0;k<2;k++)
     {
-        double* hphy_buff_send=(double *) calloc(size_x/NbLi,sizeof(double)*size_y/NbCol);
-        double* hphy_buff_recv=(double *) calloc(size_x,sizeof(double)*size_y);
-        printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
+        double* hphy_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
+        double* hphy_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
+        //printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
         for(i=0;i<size_x/NbLi;i++)//construction de buffer ligne par ligne
         {
             memcpy(hphy_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
         }
-        printf("P#%d-------End for-------\n",my_rank);
+        //printf("P#%d-------End for-------\n",my_rank);
         MPI_Gather(hphy_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE
                    ,hphy_buff_recv//&HFIL(t, (my_rank%NbCol)*size_y/NbCol,(my_rank%NbLi)*size_x/NbLi)
                    ,size_y/NbCol*size_x/NbLi,MPI_DOUBLE,0,MPI_COMM_WORLD);
-        printf("P#%d-------End gather-------\n",my_rank);
+        //printf("P#%d-------End gather-------\n",my_rank);
         if(my_rank==0)
         {
             for(i=0;i<size_x/NbLi;i++)
@@ -471,10 +471,10 @@ void forward_bloc(void) {
                 }
             }
         }
-        printf("P#%d-------End if-------\n",my_rank);
+        //printf("P#%d-------End if-------\n",my_rank);
         free(hphy_buff_send);
         free(hphy_buff_recv);
-        printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
+        //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
     }
 	if(my_rank==0)
 	{

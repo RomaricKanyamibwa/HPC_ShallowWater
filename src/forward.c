b4761f93c906239f7c6eb99416801fbf568fc988
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-05 02:30:53 +0200

    Gathering bloc v19

--- src/forward.c
+++ src/forward.c
@@ -481,8 +481,8 @@ void forward_bloc(void) {
 //                    printf("(j mod NbCol)*size_y/NbCol:%d\n",(j%NbCol)*size_y/NbCol);
 //                    printf("j*size_y/NbCol*size_x/NbLi:%d\n",j*size_y/NbCol*size_x/NbLi);
 //                    printf("i*size_y/NbCol:%d\n",i*size_y/NbCol);
-                    memcpy(&HFIL(t, i+(j/NbCol)*size_x/NbLi,(j%NbCol)*size_y/NbCol+55)
-                           ,hphy_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
+                    memcpy(&HFIL(t, i+(j/NbCol)*size_x/NbLi,(j%NbCol)*size_y/NbCol)
+                           ,hphy_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol+40);
                 }
             }
             //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);

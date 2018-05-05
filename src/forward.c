3bda63994cc60ac9879847542d6fa9fdd79e8d5a
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-05 21:03:05 +0200

    testing Gathering bloc with send and recieve v8

--- src/forward.c
+++ src/forward.c
@@ -177,8 +177,8 @@ void forward_bloc(void) {
   double* vfil_recv=(double *) calloc(size_x/NbLi,sizeof(double));
 
   unsigned char connect_msg=0;
-  double* hphy_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
-  double* hphy_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
+  double* hfil_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
+  double* hfil_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
 
   if(my_rank==0)
   {
@@ -462,10 +462,10 @@ void forward_bloc(void) {
 ////        ,&HFIL(t, i+(my_rank/NbCol)*size_x/NbLi,(my_rank%NbCol)*size_y/NbCol),size_y/NbCol/*(local_size_x-1-1*(my_rank!=0 && my_rank!=NP-1))*/,MPI_DOUBLE,0,MPI_COMM_WORLD);
 //
 //    }
-    printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
+    //printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
     for(i=0;i<size_x/NbLi;i++)//construction de buffer ligne par ligne
     {
-        memcpy(hphy_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
+        memcpy(&hfil_buff_send[i*size_y/NbCol],&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
     }
     if(my_rank==0)
     {
@@ -473,19 +473,19 @@ void forward_bloc(void) {
         {
             //printf("Root receiving\n");
             MPI_Recv(&connect_msg,1,MPI_UNSIGNED_CHAR,MPI_ANY_SOURCE,TAG_REQ,MPI_COMM_WORLD,&status);
-            printf("Root receiving2---> from P#%d\n",status.MPI_SOURCE);
-            MPI_Recv(hphy_buff_recv+size_x/NbLi*size_y/NbCol*status.MPI_SOURCE
+            //printf("Root receiving2---> from P#%d\n",status.MPI_SOURCE);
+            MPI_Recv(hfil_buff_recv+size_x/NbLi*size_y/NbCol*status.MPI_SOURCE
                      ,size_y/NbCol*size_x/NbLi,MPI_DOUBLE,status.MPI_SOURCE,TAG_GATHER,MPI_COMM_WORLD,&status);
             //printf("Root receiving3\n");
         }
     }
     else
     {
-        printf("P#%d:Root sending\n",my_rank);
+        //printf("P#%d:Root sending\n",my_rank);
         MPI_Send(&connect_msg,1,MPI_UNSIGNED_CHAR,0/*rank_master*/,TAG_REQ,MPI_COMM_WORLD);
-        printf("P#%d:Root sending2\n",my_rank);
-        MPI_Send(hphy_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE,0,TAG_GATHER,MPI_COMM_WORLD);
-        printf("P#%d:Root sending3\n",my_rank);
+        //printf("P#%d:Root sending2\n",my_rank);
+        MPI_Send(hfil_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE,0,TAG_GATHER,MPI_COMM_WORLD);
+        //printf("P#%d:Root sending3\n",my_rank);
     }
     if(my_rank==0)
         {
@@ -501,7 +501,7 @@ void forward_bloc(void) {
 //                    printf("j*size_y/NbCol*size_x/NbLi:%d\n",j*size_y/NbCol*size_x/NbLi);
 //                    printf("i*size_y/NbCol:%d\n",i*size_y/NbCol);
                     memcpy(&HFIL(t, i+(j/NbCol)*size_x/NbLi,(j%NbCol)*size_y/NbCol)
-                           ,hphy_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
+                           ,hfil_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
                 }
             }
             //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
@@ -510,16 +510,16 @@ void forward_bloc(void) {
         printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
 
 //    {
-//        double* hphy_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
-//        double* hphy_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
+//        double* hfil_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
+//        double* hfil_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
 //        //printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
 //        for(i=0;i<size_x/NbLi;i++)//construction de buffer ligne par ligne
 //        {
-//            memcpy(hphy_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
+//            memcpy(hfil_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
 //        }
 //        //printf("P#%d-------End for-------\n",my_rank);
-//        MPI_Gather(hphy_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE
-//                   ,hphy_buff_recv//&HFIL(t, (my_rank%NbCol)*size_y/NbCol,(my_rank%NbLi)*size_x/NbLi)
+//        MPI_Gather(hfil_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE
+//                   ,hfil_buff_recv//&HFIL(t, (my_rank%NbCol)*size_y/NbCol,(my_rank%NbLi)*size_x/NbLi)
 //                   ,size_y/NbCol*size_x/NbLi,MPI_DOUBLE,0,MPI_COMM_WORLD);
 //        //printf("P#%d-------End gather-------\n",my_rank);
 //        if(my_rank==0)
@@ -536,17 +536,17 @@ void forward_bloc(void) {
 ////                    printf("j*size_y/NbCol*size_x/NbLi:%d\n",j*size_y/NbCol*size_x/NbLi);
 ////                    printf("i*size_y/NbCol:%d\n",i*size_y/NbCol);
 //                    memcpy(&HFIL(t, i+(j/NbCol)*size_x/NbLi,(j%NbCol)*size_y/NbCol)
-//                           ,hphy_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
+//                           ,hfil_buff_recv+j*size_y/NbCol*size_x/NbLi+i*size_y/NbCol,size_y/NbCol);
 //                }
 //            }
 //            //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
 //        }
 //        //printf("P#%d-------End if-------\n",my_rank);
-//        free(hphy_buff_send);
-//        free(hphy_buff_recv);
+//        free(hfil_buff_send);
+//        free(hfil_buff_recv);
 //        //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
 //    }
-
+hFil=hfil_buff_recv;
    if(my_rank==0)
 	{
 	    if (file_export) {
@@ -582,8 +582,8 @@ void forward_bloc(void) {
   free(vfil_recv);
   free(vfil_send);
 
-  free(hphy_buff_send);
-  free(hphy_buff_recv);
+  free(hfil_buff_send);
+  //free(hfil_buff_recv);
 }
 
 

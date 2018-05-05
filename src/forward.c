aa27bf2ea3daf5384850a7ba75698a8c3ff15b0a
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-05 18:21:47 +0200

    testing Gathering blocv3

--- src/forward.c
+++ src/forward.c
@@ -456,15 +456,13 @@ void forward_bloc(void) {
 //    }
 
     {
-        double* hphy_buff_send=(double *) calloc(size_x*size_y,sizeof(double));
+        double* hphy_buff_send=(double *) calloc(size_x/NbLi*size_y/NbCol,sizeof(double));
         double* hphy_buff_recv=(double *) calloc(size_x*size_y,sizeof(double));
-        hphy_buff_send=&HFIL_LOCAL(t,0,0);
         //printf("P#%d:---------------------------- Magic The Gathering ----------------------------\n",my_rank);
-//        for(i=0;i<size_x/NbLi;i++)//construction de buffer ligne par ligne
-//        {
-//            memcpy(hphy_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
-//        }
-
+        for(i=0;i<size_x/NbLi;i++)//construction de buffer ligne par ligne
+        {
+            memcpy(hphy_buff_send+i*size_y/NbCol,&HFIL_LOCAL(t,i+(my_rank>=NbCol), (my_rank%NbCol!=0)),size_y/NbCol);
+        }
         //printf("P#%d-------End for-------\n",my_rank);
         MPI_Gather(hphy_buff_send,size_y/NbCol*size_x/NbLi,MPI_DOUBLE
                    ,hphy_buff_recv//&HFIL(t, (my_rank%NbCol)*size_y/NbCol,(my_rank%NbLi)*size_x/NbLi)
@@ -490,7 +488,7 @@ void forward_bloc(void) {
             //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
         }
         //printf("P#%d-------End if-------\n",my_rank);
-        //free(hphy_buff_send);
+        free(hphy_buff_send);
         free(hphy_buff_recv);
         //printf("P#%d:---------------------------- End of The Gathering ----------------------------\n",my_rank);
     }

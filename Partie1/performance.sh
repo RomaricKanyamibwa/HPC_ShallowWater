4b5c595209aa20d2771af2161778da47892f72a1
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2018-05-20 17:35:36 +0200

    test de perf

--- Partie1/performance.sh
+++ Partie1/performance.sh
@@ -3,69 +3,147 @@
 
 echo "-----------------Test 1024*1024-----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
+../../Version_Seq/bin/shalw  -x 1024 -y 1024 -t 40
+
+echo "****Decomposition par Bande****" >> perform.txt
+
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --bloc
+##mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --bloc
 
 echo "-----------------Non-Blocking Test-----------------" >> perform.txt
+
+echo "****Decomposition par Bande****" >> perform.txt
 
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 1024 -y 1024 -t 40 --non_block_comm --bloc
 
 echo "-----------------Test 2048*2048-----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
-mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
+echo "****Decomposition par Bande****" >> perform.txt
+
+../../Version_Seq/bin/shalw  -x 2048 -y 2048 -t 40
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40  --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --bloc
 
 echo "-----------------Non-Blocking Test-----------------" >> perform.txt
+
+echo "****Decomposition par Bande****" >> perform.txt
 
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 2048 -y 2048 -t 40 --non_block_comm --bloc
 
 echo "-----------------Test 4096*4096-----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
+echo "****Decomposition par Bande****" >> perform.txt
+
+../../Version_Seq/bin/shalw  -x 4096 -y 4096 -t 20
+
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --bloc
 
 echo "-----------------Non-Blocking Test-----------------" >> perform.txt
+
+echo "****Decomposition par Bande****" >> perform.txt
 
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 4096 -y 4096 -t 20 --non_block_comm --bloc
 
 echo "-----------------Test 8192*8192-----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 
+echo "****Decomposition par Bande****" >> perform.txt
+
+../../Version_Seq/bin/shalw  -x 8192 -y 8192 -t 20 
+
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --bloc
 
 echo "-----------------Non-Blocking Test-----------------" >> perform.txt
+
+echo "****Decomposition par Bande****" >> perform.txt
 
 mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm
 mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm
 mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm
-mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm
+
+echo "****Decomposition par Block****" >> perform.txt
+
+mpirun -n 2 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm --bloc
+mpirun -n 4 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm --bloc
+mpirun -n 8 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm --bloc
+#mpirun -n 16 -hostfile hostfile -bynode ./bin/shalw  -x 8192 -y 8192 -t 20 --non_block_comm --bloc
+
+
 
 echo "-----------------MPI_IO Test-----------------" >> perform.txt
 
 echo "-----------------t =40 -----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw --export -x 512 -y 512 -t 40
+../../Version_Seq/bin/shalw --export -x 512 -y 512 -t 40
 
 rm -f shalw_*
 
@@ -89,7 +167,7 @@ rm -f shalw_*
 
 echo "-----------------t =80 -----------------" >> perform.txt
 
-mpirun -n 1 -hostfile hostfile -bynode ./bin/shalw --export -x 512 -y 512 -t 80
+../../Version_Seq/bin/shalw --export -x 512 -y 512 -t 80
 
 rm -f shalw_*
 
--- Partie2/OpenMp+MPI/test.sh
+++ Partie2/OpenMp+MPI/test.sh
@@ -1,8 +1,9 @@
 
 echo "-----------------Sans export-----------------" >> perform.txt
 
-../../../Version_Seq/bin/shalw   -x 2048 -y 2048 -t 80
+echo "-----------------Test 2048*2048-----------------" >> perform.txt
 
+../../../Version_Seq/bin/shalw   -x 2048 -y 2048 -t 80
 
 mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc  -x 2048 -y 2048 -t 80
 mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  -x 2048 -y 2048 -t 80
@@ -13,6 +14,8 @@ mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc  -x 2048 -y 
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  -x 2048 -y 2048 -t 80
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc --non_block_comm  -x 2048 -y 2048 -t 80
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --non_block_comm  -x 2048 -y 2048 -t 80
+
+echo "-----------------Test 4096*4096-----------------" >> perform.txt
 
 ../../../Version_Seq/bin/shalw   -x 4096 -y 4096 -t 80
 
@@ -25,6 +28,20 @@ mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc  -x 4096 -y 
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  -x 4096 -y 4096 -t 80
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc --non_block_comm  -x 4096 -y 4096 -t 80
 mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --non_block_comm  -x 4096 -y 4096 -t 80
+
+echo "-----------------Test 8192*8192-----------------" >> perform.txt
+
+../../../Version_Seq/bin/shalw   -x 8192 -y 8192 -t 20
+
+mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc  -x 8192 -y 8192 -t 20
+mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
+mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc --non_block_comm  -x 8192 -y 8192 -t 20
+mpirun -n 2 -bynode -hostfile hostfile  -bynode ./bin/shalw  --non_block_comm  -x 8192 -y 8192 -t 20
+
+mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc  -x 8192 -y 8192 -t 20
+mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  -x 8192 -y 8192 -t 20
+mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --bloc --non_block_comm  -x 8192 -y 8192 -t 20
+mpirun -n 4 -bynode -hostfile hostfile  -bynode ./bin/shalw  --non_block_comm  -x 8192 -y 4096 -t 20
 
 
 # echo "-----------------Avec export-----------------" >> perform.txt
